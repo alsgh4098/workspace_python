01829ca347c4097d247d59ab9d760a272f926a67
A-12 <alsgh4098@naver.com>
2019-09-07 17:19:11 +0900

    Start_Commit!

--- venv/mysite2/mysite2/urls.py
+++ venv/mysite2/mysite2/urls.py
@@ -17,19 +17,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('blog/', include('blog.urls')),
     path('board/', include('board.urls')),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
-
-# < h1
-# style = "width: 100%; height: 200px; font-size: 20px; line-height: 18px; border: 1px solid #dddddd; padding: 20px;" >
-# 제목: {{KEY_DETAIL
-# .0
-# .1}}
-# < / h1 >
-#
-# < pre
-# style = "width: 100%; height: 200px; font-size: 20px; line-height: 18px; border: 1px solid #dddddd; padding: 20px;" >
-# 내용:
-# < / pre >
